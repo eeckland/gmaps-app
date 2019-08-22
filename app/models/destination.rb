548a2608e8eb2dc412fba344853d9e7941bf720a
Emily Eckland <eecklan.com>
2019-08-22 08:48:01 -0500

    created destination model and installed geocoder gem

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,2 +1,8 @@
-class Destination < ApplicationRecord
+class Destination < ActiveRecord::Base
+  geocoded_by :address
+  after_validation :geocode
+
+  def address
+    "#{city}, #{country}"
+  end
 end
